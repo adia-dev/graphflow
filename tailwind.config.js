64504f89136a673b58ffb336a0808bec68c2b570
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-03-31 02:20:10 +0200

    Installed and configured TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#eef2ff",
+          100: "#e0e7ff",
+          200: "#c7d2fe",
+          300: "#a5b4fc",
+          400: "#818cf8",
+          500: "#6366f1",
+          600: "#4f46e5",
+          700: "#4338ca",
+          800: "#3730a3",
+          900: "#312e81",
+        },
+        secondary: {
+          50: "#ecfdf5",
+          100: "#d1fae5",
+          200: "#a7f3d0",
+          300: "#6ee7b7",
+          400: "#34d399",
+          500: "#10b981",
+          600: "#059669",
+          700: "#047857",
+          800: "#065f46",
+          900: "#064e3b",
+        },
+        "dark-primary": "#161819",
+        "dark-secondary": "#1F2125",
+        "dark-tertiary": "#2C2F36",
+        "background-popup": "#1F2125B0",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+  // darkMode: "class", // dark mode by default
+};
