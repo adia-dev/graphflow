9c499da55d83a2197e28786c6f7b2d8f7d96eecb
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-07-01 15:59:48 +0200

    Colored cursor label

--- src/features/Cursor/index.tsx
+++ src/features/Cursor/index.tsx
@@ -8,17 +8,26 @@ const Cursor = (props: Props) => {
 
   return (
     <div
-      className="absolute z-0 w-4 h-4 pointer-events-none"
+      className="absolute z-0 w-4 h-4 pointer-events-none dark:text-white"
       style={{
         left: `${cursor.position.x}px`,
         top: `${cursor.position.y}px`,
-        color: `${cursor.color || "#fff"}`,
       }}
     >
-      <div className="whitespace-nowrap text-xs absolute top-6 left-4">
+      <div
+        className="whitespace-nowrap text-xs absolute top-6 left-4 px-2 py-1 rounded-full"
+        style={{
+          backgroundColor: `${cursor.color || "#fff"}`,
+        }}
+      >
         {cursor.label}
       </div>
-      <BsCursorFill className="-rotate-90 absolute top-full left-0" />
+      <BsCursorFill
+        className="-rotate-90 absolute top-full left-0"
+        style={{
+          color: `${cursor.color || "#fff"}`,
+        }}
+      />
     </div>
   );
 };
