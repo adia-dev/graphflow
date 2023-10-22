13079fbe90618c57cc71529c0a6d881f32a85159
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-10-22 23:57:34 +0200

    I need to finish this project

--- src/features/Context/Help/index.tsx
+++ src/features/Context/Help/index.tsx
@@ -2,7 +2,7 @@
 type Props = {};
 
 const ContextualHelp = (props: Props) => {
-  return <div className="absolute z-20 bg-white">ContextualHelp</div>;
+  return <div className="absolute z-20 bg-white"></div>;
 };
 
 export default ContextualHelp;
